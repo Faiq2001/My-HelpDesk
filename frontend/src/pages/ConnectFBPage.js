166cf5763d2cb32a758011c0dc1f0eb469f24496
Faiq2001 <faiqahmadmalik@gmail.com>
2024-02-28 11:46:06 +0530

    Correcting ConnectFBPage.js

--- frontend/src/pages/ConnectFBPage.js
+++ frontend/src/pages/ConnectFBPage.js
@@ -1,8 +1,29 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 
 const ConnectFBPage = () => {
   const history = useHistory();
+
+  useEffect(() => {
+    // Load the Facebook SDK asynchronously
+    window.fbAsyncInit = function () {
+      window.FB.init({
+        appId: '788371886528778', // Replace with your Facebook App ID
+        cookie: true,
+        xfbml: true,
+        version: 'v19.0' // Use the version of the Graph API you want to use
+      });
+    };
+
+    // Load the SDK asynchronously
+    (function (d, s, id) {
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) return;
+      js = d.createElement(s); js.id = id;
+      js.src = "https://connect.facebook.net/en_US/sdk.js";
+      fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+  }, []);
 
   const handleConnect = () => {
     // Initiate Facebook login process
@@ -16,7 +37,7 @@ const ConnectFBPage = () => {
           console.log('User cancelled login or did not fully authorize.');
         }
       });
-    };
+    }
   };
 
   return (
